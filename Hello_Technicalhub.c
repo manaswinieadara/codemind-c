836309b08b3027a7f75d52773b5dbe6aa9be9761
manaswinieadara <96780506+manaswinieadara@users.noreply.github.com>
2022-06-29 07:09:21 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[200]={"Hello Technicalhub"};     char p[200];     scanf("%[^ ]s",p);     printf("%s ",str);     printf("%s",p); }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[200]={"Hello Technicalhub"};
+    char p[200];
+    scanf("%[^
+]s",p);
+    printf("%s
+",str);
+    printf("%s",p);
+}
